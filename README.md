a205880db991ba2eb60ab8ec5601a032609b5bc1
Mustafa Eker <mustafaekr@gmail.com>
2018-08-08 14:08:19 +0300

    nearly completed

--- README.md
+++ README.md
@@ -1,4 +1,33 @@
-# Dynamic-JS-List-ex.
+# Dynamic-JS-List
+
 <br><br>
-Dynamic list Exercise (items can be added/deleted dynamicly with the help of JS)
+ ------INSTANT TO DO LIST ------
 <br><br>
+JS - Dunamic List Exercise 
+<br><br>
+<br><br>
+* "To Do"s can be added by add button
+<br><br>
+* "To Do"s can be deleted by "x" icon of relevant row.
+<br><br>
+* "To Do"s can be mark as "completed" by clicking over.
+<br><br>
+* Completed "To Do"s can be deleted by "Delete All Completed" button.
+<br><br>
+* Editing "script.js" routine "To Do"s can be delared automaticly at each refresh.
+<br><br>
+<br><br>
+-----------------------
+<br><br>
+In Schedule:
+<br><br>
+1-blue focus border will be deleted.
+<br><br>
+2-Add button focus border will be deleted.
+<br><br>
+3-opacity settings eill be optimized.
+<br><br>
+4-After "Add" click input text will be cleaned auto.
+<br><br>
+5- ... any more suggestion ?
+![alt text](https://raw.githubusercontent.com/mustafaeker/Dynamic-JS-List-ex./blob/master/read-me.png)
--- script.js
+++ script.js
@@ -16,104 +16,82 @@ list.innerHTML=html;
 **************************************************/
 
 
-
-/***************** DAILY DYNAMIC TO DO LIST ************************************/
-
-/******* DEFINE DEFAULT DAILY TODOS  **********/ 
-
-var items = ['work todo 1', 'work todo 2', 'work todo 3', 'work todo 4'];
+var items = ['work to do 1', 'work to do 2', 'work to do 3', 'work to do 4'];
 
 /******* DEFINE DEFAULT DAILY TODOS  *********
 var myspandate = document.querySelector('#date');
 
     var date = new Date(year, month, day, hours, minutes, seconds, milliseconds);
     var myspantext = date.getDate();
-var myspandate = date.getDate();*/ 
+var myspandate = date.getDate();*/
 
 
-/******* CRETAE A TODO FOR LIST  **********/ 
+/******* CRETAE A TODO FOR LIST  **********/
 
-var list = document.querySelector('#mylist');
-
-items.forEach(function (item){
-    
-    CretaeItem(item);
-});
-
-
-
-document.querySelector('#add').onclick=function(){
-    
-  var item = document.querySelector('#txtItem').value;
-   
-    
-    if((item==='') || (item==0)){
-     alert('Nothing to Do !');
-        return;
-    }
-    CretaeItem(item);
-};
-
-function CretaeItem(item){
+function CreateItem(item) {
     var li = document.createElement('li');
     var t = document.createTextNode(item);
     var span = document.createElement('span');
-    var spantext = document.createTextNode ('\u00D7');
-    
-    li.className='list-group-item';
+    var spantext = document.createTextNode('\u00D7');
+
+    li.className = 'list-group-item';
     li.appendChild(t); /****** "li" is a parent element of this "t" *********/
     list.appendChild(li); /*** list "<ul>" is a parent element of this "li" */
     span.classList.add('close');
     span.appendChild(spantext);
     li.appendChild(span);
-      
+
     span.onclick = function () {
         var li = this.parentElement;
-        li.style.display='none';  
-        li.classList.remove('checked');   
+        li.style.display = 'none';
+        li.classList.remove('checked');
     }
 };
 
 
+var list = document.querySelector('#mylist');
 
-/******* MAKES A TODO'S STATE "COMPLETED" **********/ 
+items.forEach(function (item) {
 
-list.addEventListener('click',function(item){
-    
-    if(item.target.tagName=='LI'){
-        item.target.classList.toggle('checked');
-        ToggleDeleteButton();
-        
-    }
-    
+    CreateItem(item);
 });
 
+document.querySelector('#add').onclick = function () {
 
-document.querySelector('#deleteAll').onclick=function(){
-  
-    var elements = document.querySelector('.checked');
-    
-    elements.forEach(function(item){
-        item.style.display='none'
-    })
-};
+    var item = document.querySelector('#txtItem').value;
 
 
-
-function ToggleDeleteButton(){
-  var checklist = document.querySelectorAll('.list-group-item.checked');
-    
-    if(checklist.length>0){
-        document.querySelector('#deleteAll').classList.remove('.d-none');
-        
-    }else{
-         document.querySelector('#deleteAll').classList.add('.d-none');
+    if ((item === '') || (item == 0)) {
+        alert('Nothing to Do !');
+        return;
     }
+    CreateItem(item);
 };
 
 
 
 
+
+
+
+/******* TODO'S STATE "COMPLETED" **********/
+
+list.addEventListener('click', function (item) {
+
+    if (item.target.tagName == 'LI') {
+        item.target.classList.toggle('checked');
+        ////////// DELETE BUTTON APPEAR/DISAPPEAR ///////
+        var checkList = document.querySelectorAll('.checked');
+        console.log(checkList.length)
+
+        if (checkList.length > 0) {
+            document.querySelector('#deleteAll').classList.remove('d-none');
+        } else {
+            document.querySelector('#deleteAll').classList.add('d-none');
+        }
+    }
+
+});
 
 
 
